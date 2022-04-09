507e13fe265fcc5eedb0ce4aa4f6fae19121a4c8
Siddhant <santoshshrivastava39@gmail.com>
2022-04-09 15:05:20 +0530

    Create a Address Book Table

--- AddressBookService.sql
+++ AddressBookService.sql
@@ -2,3 +2,19 @@
 create database AddressBookService;
 
 use AddressBookService;
+
+--UC2 Ability to create a Address Book Table 
+create table AddressBook
+(
+Id int,
+FirstName varchar(255),
+LastName varchar(255),
+Address varchar(300),
+City varchar(20),
+State varchar(20),
+ZipCode int ,
+PhoneNo bigint,
+EmailId varchar(255)
+)
+
+select * from AddressBook
