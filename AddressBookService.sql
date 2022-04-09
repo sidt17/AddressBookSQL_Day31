0d6ff95d93f440edb313dd8a38b1b4a883891b02
Siddhant <santoshshrivastava39@gmail.com>
2022-04-09 15:21:44 +0530

    Insert new contacts in Address Book

--- AddressBookService.sql
+++ AddressBookService.sql
@@ -10,11 +10,18 @@ Id int,
 FirstName varchar(255),
 LastName varchar(255),
 Address varchar(300),
-City varchar(20),
-State varchar(20),
+City varchar(355),
+State varchar(300),
 ZipCode int ,
 PhoneNo bigint,
 EmailId varchar(255)
 )
 
 select * from AddressBook
+
+--UC3 Insert contacts in table
+insert into AddressBook values(1,'Rohit','Jain','A nagar','Indore','MP',435353,9687685854,'Rohit@gmail.com');
+insert into AddressBook values(2,'Amit','Kumawat','B nagar','Jaipur','Rajasthan',462506,9988772234,'Amit@gmail.com');
+insert into AddressBook values(3,'Sam','Khan','xy road','Bhopal','MP',434406,9923877223,'Sam@gmail.com');
+insert into AddressBook values(4,'Atul','Anjana','DE block','Chennai','Tamil Nadu',422406,9988672234,'niAtulkita@gmail.com');
+insert into AddressBook values(5,'Rishabh','Patel',' Mira Road','Dehradun','Uttarakhand',455406,9448772234,'Rishabh@gmail.com');
