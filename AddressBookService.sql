4c174a495ba35bf0956f711df72f1253415f8185
Siddhant <santoshshrivastava39@gmail.com>
2022-04-09 16:26:19 +0530

    Retrieve enteries sorted alphabetically

--- AddressBookService.sql
+++ AddressBookService.sql
@@ -41,3 +41,13 @@ where State= 'MP' or State = 'Rajasthan'
 --UC7 Count size of addressbook by city and state
 select count(City) as TotalCity from AddressBook;
 select count(State) as TotalState from AddressBook;   
+
+--UC8 Ability to retrieve entries sorted  alphabetically by person's name for a given city
+insert into AddressBook values(1,'Rohit','Jain','A nagar','Indore','MP',435353,9687685854,'Rohit@gmail.com');
+insert into AddressBook values(2,'Amit','Kumawat','B nagar','Jaipur','Rajasthan',462506,9988772234,'Amit@gmail.com');
+insert into AddressBook values(3,'Sam','Khan','xy road','Bhopal','MP',434406,9923877223,'Sam@gmail.com');
+insert into AddressBook values(4,'Atul','Anjana','DE block','Chennai','Tamil Nadu',422406,9988672234,'niAtulkita@gmail.com');
+insert into AddressBook values(5,'Rishabh','Patel',' Mira Road','Dehradun','Uttarakhand',455406,9448772234,'Rishabh@gmail.com');
+
+select * from AddressBook  order by FirstName asc;	--ascending
+select * from AddressBook  order by FirstName desc; --descending
